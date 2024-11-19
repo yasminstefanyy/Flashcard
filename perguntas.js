a5ec289288e73a362280059ba7e5c539f0eddb7b
yasminstefanyy <00001085347473sp@al.educacao.sp.gov.br>
2024-11-19 18:59:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Quimica',
+    'Atualmente, quantos elementos químicos a tabela periódica possui??',
+    '118'
+)
+
+criaCartao(
+    'Ed. Fisica',
+    'Qual o número mínimo de jogadores em cada time numa partida de futebol??',
+    'Uma partida de futebol conta com o número máximo de 11 jogadores e mínimo de 7, contando com o goleiro, em cada equipe.'
+)
+
+criaCartao(
+    'Fato',
+    'Quem criou esse Projeto?',
+    'Yasmin :) '
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'O que a palavra legend significa em português?',
+    'Lenda'
+)
